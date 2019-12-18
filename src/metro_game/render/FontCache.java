6f249c8fdcf60df3e9666a57bf8722ede53df483
Kirill Surkov <ksurkov@ab-technology.ru>
2019-12-18 06:12:37 +0300

    Add font cache

---
+++ src/metro_game/render/FontCache.java
@@ -0,0 +1,34 @@
+package metro_game.render;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import metro_game.Context;
+
+public class FontCache {
+	private Context m_context;
+	private Map<String, Map<Integer, Font>> m_cache;
+	
+	public FontCache(Context context) {
+		m_context = context;
+		m_cache = new HashMap<String, Map<Integer, Font>>();
+	}
+	
+	public Font getFont(String name, int size) {
+		Map<Integer, Font> sizeCache = null;
+		if (m_cache.containsKey(name)) {
+			sizeCache = m_cache.get(name);
+		} else {
+			sizeCache = new HashMap<Integer, Font>();
+			m_cache.put(name, sizeCache);
+		}
+		Font cachedFont = null;
+		if (sizeCache.containsKey(size)) {
+			cachedFont = sizeCache.get(size);
+		} else {
+			cachedFont = new Font(m_context, name, size);
+			sizeCache.put(size, cachedFont);
+		}
+		return cachedFont;
+	}
+}
